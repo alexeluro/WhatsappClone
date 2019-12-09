22deb2f4f1dd0245e2ad2a03a071872bf2984a45
alexeluro <alexeluro@gmail.com>
2019-12-09 03:54:06 +0100

    Initial commit

---
+++ app/src/main/java/com/example/whatsappclone/CallViewModel.java
@@ -0,0 +1,7 @@
+package com.example.whatsappclone;
+
+import androidx.lifecycle.ViewModel;
+
+public class CallViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/example/whatsappclone/StatusViewModel.java
@@ -0,0 +1,7 @@
+package com.example.whatsappclone;
+
+import androidx.lifecycle.ViewModel;
+
+public class StatusViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
